1d6723026cd443229f13cb4dd4f5de0e4f2f71cf
AkshayGohrava <akshaygohrava@gmail.com>
2024-11-08 16:06:23 +0530

    An NetFlix-Clone

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <>
   <h1 className="text-4xl font-bold mb-7 pb-2 text text-red-500">NetFlix</h1>
-  <ul className='flex flex-row gap-8 flex-wrap justify-around'>
+  <ul className='net-ul flex flex-row gap-8 flex-wrap justify-around'>
    {series.map((props) => (
-    <li key={props.id} className='flex flex-col gap-5 justify-start align-middle'>
+    <li key={props.id} className='netflix-list flex flex-col gap-5 justify-start align-middle'>
       <img src={props.img_url} alt={props.name}></img>
       <h1>Series Name: {props.name}</h1>
       <h2>Title: {props.id}</h2>
@@ -18,12 +18,38 @@ function App() {
       <button className='mb-5'><a href={props.watch_url}>Watch Now</a></button>
     </li>
    ))}
-   <footer className='relative bottom-0 bg-stone-900 w-full h-60'>
-    <div className='mt-8'>
-    <input type='email' className='input-email' placeholder='Email address' required></input>
-    <button className='netflix-button'>Get Started</button>
+   <form className='mt=7'>
+   <input type='email' className='input-email' placeholder='Email address' required></input>
+   <button className='netflix-button'>Get Started</button>
+   </form>
+   <footer className='relative bottom-0 bg-stone-900 p-5'>
+    <p>Questions? Call </p><span className='underline'>000-800-919-1694</span>
+    <div className='flex flex-row justify-center align-middle mt-5 gap-32 sm:flex-row'>
+    <ul className='flex flex-col gap-1'>
+      <li>FAQ</li>
+      <li>Investor Relations</li>
+      <li>Privacy</li>
+      <li>Speed Test</li>
+    </ul>
+    <ul className='flex flex-col gap-1'>
+      <li>Help Centre</li>
+      <li>Jobs</li>
+      <li>Cookie Preferences</li>
+      <li>Legal Notices</li>
+    </ul>
+    <ul className='flex flex-col gap-1'>
+      <li>Account</li>
+      <li>Ways to Watch</li>
+      <li>Corporate Information</li>
+      <li>Only on Netflix</li>
+    </ul>
+    <ul className='flex flex-col gap-1'>
+      <li>Media Centre</li>
+      <li>Terms of Use</li>
+      <li>Contact Us</li>
+    </ul>
     </div>
-    
+   
    </footer>
   </ul>
     </>
